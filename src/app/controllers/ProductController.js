95d859395cefe766897cc9dc835c089adbdda043
matheusgir <girardi.matheus@hotmail.com>
2020-07-18 18:48:35 -0300

    product controller

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,11 +1,12 @@
-const Category = require('../models/Category')
+const Category = require('../models/Category');
+const Product = require('../models/Product');
 
 module.exports = {
     create(req, res) {
         //Pegar Categorias
         Category.all()
         .then(function(results){
-            const categories = results.rows;
+            const categories = results.rows
 
             return res.render("products/create.njk", { categories })
 
@@ -13,7 +14,23 @@ module.exports = {
             throw new Error(err)
         })
     },
-    post(req, res) {
-        //Lógica de Salvar
+    async post(req, res) {
+        //logica de salvar
+       const keys = Object.keys(req.body)
+
+       for(key of keys) {
+           if(req.body[key] == "") {
+               return res.send('Please, fill all fields')
+           }
+       }
+
+       let results = await Product.create(req.body)
+       const productId = results.rows[0]
+
+       results = await Category.all()
+       const categories = results.rows
+
+       return res.render("products/create.njk", {productId, categories})
+       
     }
 }
